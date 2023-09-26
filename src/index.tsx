1aec4f7fd6d9d89eb96e8d6f07310cc216c0bad6
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-09-25 17:00:46 -0700

    feat: consolidate components

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,38 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
+import type { Component } from "solid-js";
+import "./index.css";
+import styles from "./App.module.css";
 
-import './index.css';
-import App from './App';
+import logo from "./logo.svg";
 
-const root = document.getElementById('root');
+const Index: Component = () => {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+};
+
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
   );
 }
 
-render(() => <App />, root!);
+render(() => <Index />, root!);
