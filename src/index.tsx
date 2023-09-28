21766e5de22fdc2c9e0ec24b6d15ad344aa20d5d
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-09-27 17:07:54 -0700

    feat: consolidate tsx files into one

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,43 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
-import App from './App';
+// Assets
+import logo from "./logo.svg";
 
-const root = document.getElementById('root');
+// CSS
+import styles from "./App.module.css";
+import "./index.css";
+
+// Types
+import type { Component } from "solid-js";
+
+const Index: Component = () => {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+};
+
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
   );
 }
 
-render(() => <App />, root!);
+render(() => <Index />, root!);
--- todo.md
+++ todo.md
@@ -1,6 +1,5 @@
 # To Do
 
-- Consolidate index.tsx/App.tsx file
 - Consolidate css files
 - Manifest with crxjs ts file
 - Look at Solid devtools?
