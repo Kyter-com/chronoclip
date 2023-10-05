f926daece962a4881dbf90bfb076c2f0061fa010
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-10-05 16:51:41 -0700

    feat: solid devtools todo

--- todo.md
+++ todo.md
@@ -1,5 +1,4 @@
 # To Do
 
 - TODO: Hover to copy with little popup. Copy on click
-- TODO: Look at Solid devtools?
 - TODO: Playwright automated testing?
--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,9 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest";
-// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-    crx({ manifest }),
-  ],
+  plugins: [solidPlugin(), crx({ manifest })],
   server: {
     port: 3000,
   },
