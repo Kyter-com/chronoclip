5d0b38ad8cdace19766cd0813930644056da1fea
Nick Reisenauer <nickdreisenauer@gmail.com>
2023-10-06 15:51:24 -0700

    feat: setup vite image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import { crx } from "@crxjs/vite-plugin";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import manifest from "./src/manifest";
 
 export default defineConfig({
-  plugins: [solidPlugin(), crx({ manifest })],
+  plugins: [solidPlugin(), crx({ manifest }), ViteImageOptimizer()],
   server: {
     port: 3000,
   },
